d9ca20e43b211342386eb37ec882262b489b8e71
Isaac Adewumi <ienioladewumi@gmail.com>
2023-09-17 09:24:17 +0100

    feat: add drf custom exception handler, error views, utility db models & response helpers

---
+++ utils/views.py
@@ -0,0 +1,81 @@
+import logging
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from drf_yasg.generators import OpenAPISchemaGenerator
+
+from django.http import HttpRequest, JsonResponse
+
+from rest_framework.response import Response
+from rest_framework import status, serializers
+from rest_framework.permissions import AllowAny
+from rest_framework.exceptions import APIException
+from rest_framework.views import exception_handler
+
+from .responses import error_response
+
+logger = logging.getLogger(__name__)
+
+
+def custom_exception_handler(exception, context) -> Response | None:
+    if not isinstance(exception, serializers.ValidationError):
+        logger.exception(
+            'An exception occurred while handling request %s',
+            context['request'].get_full_path(),
+            exc_info=exception,
+        )
+
+    response = exception_handler(exception, context)
+    if response is None:
+        return None
+
+    if isinstance(exception, APIException):
+        return error_response(
+            message=exception.__class__.__name__,
+            errors=[exception.detail] if isinstance(exception.detail, str) else exception.detail,
+            status_code=response.status_code,
+        )
+
+    return error_response(
+        message=exception.__class__.__name__,
+        errors=None,
+        status_code=response.status_code,
+    )
+
+
+def handler_400(request, exception, *args, **kwargs):  # noqa: ARG001
+    return JsonResponse(data={'message': 'Bad request', 'errors': None}, status=status.HTTP_400_BAD_REQUEST)
+
+
+def handler_404(request, exception):  # noqa: ARG001
+    return JsonResponse(data={'message': 'Not found', 'errors': None}, status=status.HTTP_404_NOT_FOUND)
+
+
+def handler_500(request: HttpRequest) -> JsonResponse:  # noqa: ARG001
+    return JsonResponse(
+        data={
+            'message': "We're sorry, but something went wrong on our end",
+            'errors': None,
+        },
+        status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
+
+
+class HttpAndHttpsOpenAPISchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):  # noqa: FBT002, PLR6301
+        schema = super().get_schema(request, public)
+        schema.schemes = ['http', 'https']
+        return schema
+
+
+docs_schema_view = get_schema_view(
+    openapi.Info(
+        title='BridgeBloc API',
+        default_version='v1',
+        description='BridgeBloc API',
+        license=openapi.License(name='MIT License'),
+    ),
+    generator_class=HttpAndHttpsOpenAPISchemaGenerator,
+    public=True,
+    permission_classes=[AllowAny],
+)
