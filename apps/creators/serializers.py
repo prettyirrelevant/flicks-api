efa3bcf1057806e642bf0cbc86ed28f3919317b1
Isaac Adewumi <ienioladewumi@gmail.com>
2023-10-11 07:51:55 +0100

    feat: add endpoint to create creators

--- apps/creators/serializers.py
+++ apps/creators/serializers.py
@@ -1,4 +1,12 @@
+from solders.pubkey import Pubkey
+
+from django.conf import settings
+
 from rest_framework import serializers
+
+from apps.subscriptions.choices import SubscriptionType
+
+from services.sharingan import SharinganService
 
 from .models import Wallet, Creator, WalletDepositAddress
 
@@ -24,13 +32,54 @@ class CreatorSerializer(serializers.ModelSerializer):
         model = Creator
         fields = (
             'id',
+            'biowallet',
             'address',
-            'email',
-            'wallet',
             'moniker',
-            'is_verified',
-            'is_suspended',
-            'suspension_reason',
             'created_at',
             'updated_at',
+            'is_verified',
+            'social_linksis_suspended',
+            'suspension_reason',
         )
+
+
+class CreatorCreationSerializer(serializers.Serializer):
+    bio = serializers.CharField(max_length=200)
+    image_url = serializers.URLField(required=True)
+    banner_url = serializers.URLField(required=True)
+    address = serializers.CharField(max_length=44, required=True)
+    moniker = serializers.CharField(max_length=5000, required=True)
+
+    def validate_address(self, value):  # noqa: PLR6301
+        public_key = Pubkey.from_string(value)
+        if not public_key.is_on_curve():
+            raise serializers.ValidationError('Invalid address provided.')
+
+        return value
+
+    def validate(self, attrs):  # noqa: PLR6301
+        sharingan_service = SharinganService(settings.SHARINGAN_BASE_URL)
+        response = sharingan_service.resolve_address_to_sns(attrs['address'])
+        if response is None and attrs['moniker'].endswith('.sol'):
+            raise serializers.ValidationError('You cannot use an SNS as a moniker if you do not own it.')
+
+        if (
+            response is not None
+            and attrs['moniker'].endswith('.sol')
+            and f'{response["domainName"]}.sol' != attrs['moniker']
+        ):
+            raise serializers.ValidationError('Moniker type of SNS selected does not belong to address.')
+
+        social_links = {}
+        is_verified = False
+        if response is not None:
+            response.pop('domainName')
+            if 'twitter' in response:
+                is_verified = True
+
+            social_links = response
+
+        attrs['is_verified'] = is_verified
+        attrs['social_links'] = social_links
+        attrs['subscription_type'] = SubscriptionType.FREE
+        return attrs
--- apps/creators/urls.py
+++ apps/creators/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CreatorAPIView
+from .views import CreatorAPIView, CreatorCreationAPIView
 
 urlpatterns = [
     path('creators/me', CreatorAPIView.as_view(), name='my-profile'),
+    path('creators', CreatorCreationAPIView.as_view(), name='create-creator'),
 ]
