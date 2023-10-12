eb0b077fc3e5ede25913a8f007a14b8fb0c40389
Isaac Adewumi <ienioladewumi@gmail.com>
2023-10-12 08:11:48 +0100

    feat: add endpoint to search for creators & fetch creator info

--- apps/creators/urls.py
+++ apps/creators/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CreatorAPIView, CreatorCreationAPIView
+from .views import CreatorAPIView, SearchCreatorsAPIView, CreatorCreationAPIView
 
 urlpatterns = [
-    path('me', CreatorAPIView.as_view(), name='my-profile'),
     path('', CreatorCreationAPIView.as_view(), name='create-creator'),
+    path('<str:address>', CreatorAPIView.as_view(), name='creator-detail'),
+    path('search', SearchCreatorsAPIView.as_view(), name='search-creators'),
 ]
--- apps/creators/views.py
+++ apps/creators/views.py
@@ -1,12 +1,21 @@
 from typing import ClassVar
 
+from django.db.models import Q
+
+from rest_framework import serializers
+from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView, RetrieveAPIView, get_object_or_404
 
 from utils.responses import success_response
 
 from .models import Creator
 from .permissions import IsAuthenticated
-from .serializers import CreatorSerializer, CreatorCreationSerializer
+from .serializers import (
+    CreatorSerializer,
+    MinimalCreatorSerializer,
+    CreatorCreationSerializer,
+    CreatorWithoutWalletSerializer,
+)
 
 
 class CreatorCreationAPIView(GenericAPIView):
@@ -22,9 +31,16 @@ class CreatorCreationAPIView(GenericAPIView):
 
 
 class CreatorAPIView(RetrieveAPIView):
-    queryset = Creator.objects.select_related('wallet').prefetch_related('wallet__deposit_addresses')
-    permission_classes = (IsAuthenticated,)
+    lookup_field = 'address'
     serializer_class = CreatorSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Creator.objects.select_related('wallet').prefetch_related('wallet__deposit_addresses')
+
+    def get_serializer_class(self):
+        if self.request.user.address == self.get_object().address:
+            return CreatorSerializer
+
+        return CreatorWithoutWalletSerializer
 
     def retrieve(self, request, *args, **kwargs):  # noqa: PLR6301
         response = super().retrieve(request, *args, **kwargs)
@@ -36,3 +52,19 @@ class CreatorAPIView(RetrieveAPIView):
         self.check_object_permissions(self.request, obj)
 
         return obj
+
+
+class SearchCreatorsAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = MinimalCreatorSerializer
+
+    def get(self, request, *args, **kwargs):  # noqa: PLR6301 ARG002
+        q = request.query_params.get('q')
+        if q is None:
+            raise serializers.ValidationError('`q` query parameter is required.')
+
+        # This can be improved to use Postgres FTS but for now this should be sufficient.
+        qs = Creator.objects.filter(Q(address__icontains=q) | Q(moniker__icontains=q))
+        serializer = MinimalCreatorSerializer(qs, many=True)
+
+        return success_response(serializer.data)
