8e0aa81f552152a4427f09c57693c12bd0812673
Isaac Adewumi <ienioladewumi@gmail.com>
2023-10-16 00:25:49 +0100

    chore: fix order of urls in creators app

--- apps/contents/urls.py
+++ apps/contents/urls.py
@@ -17,6 +17,13 @@ from .views import (
 
 urlpatterns = [
     path('', ContentView.as_view(), name='content'),
+    path('discover', DiscoverView.as_view(), name='discover-view'),
+    path('get-upload-urls', PreSignedURLView.as_view(), name='presigned-urls'),
+    path('livestreams', LivestreamView.as_view(), name='livestream-view'),
+    path('livestreams/<uuid:stream_id>', LivestreamView.as_view(), name='update-livestream-view'),
+    path('livestreams/<uuid:stream_id>/join', JoinLivestreamView.as_view(), name='join-livestream'),
+    path('timeline', TimelineView.as_view(), name='timeline-view'),
+    path('media/<str:address>', MediaView.as_view(), name='media-view'),
     path('creators/<str:address>', ContentListAPIView.as_view(), name='creators-content'),
     path('<uuid:content_id>/pay', PayForContentAPIView.as_view(), name='pay-for-content'),
     path('<uuid:content_id>/comments', CreateCommentAPIVIew.as_view(), name='create-comment'),
@@ -25,13 +32,6 @@ urlpatterns = [
         DeleteCommentAPIView.as_view(),
         name='delete-comment',
     ),
-    path('livestreams', LivestreamView.as_view(), name='livestream-view'),
-    path('discover', DiscoverView.as_view(), name='discover-view'),
-    path('media/<str:address>', MediaView.as_view(), name='media-view'),
-    path('timeline', TimelineView.as_view(), name='timeline-view'),
-    path('livestreams/<uuid:stream_id>', LivestreamView.as_view(), name='update-livestream-view'),
-    path('livestreams/<uuid:stream_id>/join', JoinLivestreamView.as_view(), name='join-livestream'),
-    path('get-upload-urls', PreSignedURLView.as_view(), name='presigned-urls'),
     path('<uuid:content_id>', ContentView.as_view(), name='content detail'),
     path('<uuid:content_id>/likes', LikesAPIView.as_view(), name='content-like-dislike'),
 ]
--- apps/creators/urls.py
+++ apps/creators/urls.py
@@ -11,8 +11,8 @@ from .views import (
 
 urlpatterns = [
     path('', CreatorCreationAPIView.as_view(), name='create-creator'),
-    path('suggestions', SuggestedCreatorAPIView.as_view(), name='suggested-creators'),
     path('search', SearchCreatorsAPIView.as_view(), name='search-creators'),
+    path('suggestions', SuggestedCreatorAPIView.as_view(), name='suggested-creators'),
     path('withdrawals', CreatorWithdrawalAPIView.as_view(), name='withdraw-money-from-creator-wallet'),
     path('moniker-availability', MonikerAvailabilityAPIView.as_view(), name='moniker-availability'),
     path('<str:address>', CreatorAPIView.as_view(), name='creator-detail'),
