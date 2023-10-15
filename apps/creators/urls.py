3e1d3ddc4b4c0b371f2b8846894d4df23a486a49
Isaac Adewumi <ienioladewumi@gmail.com>
2023-10-16 00:13:34 +0100

    chore: fix order of urls in creators app

--- apps/creators/urls.py
+++ apps/creators/urls.py
@@ -12,8 +12,8 @@ from .views import (
 urlpatterns = [
     path('', CreatorCreationAPIView.as_view(), name='create-creator'),
     path('suggestions', SuggestedCreatorAPIView.as_view(), name='suggested-creators'),
-    path('<str:address>', CreatorAPIView.as_view(), name='creator-detail'),
     path('search', SearchCreatorsAPIView.as_view(), name='search-creators'),
     path('withdrawals', CreatorWithdrawalAPIView.as_view(), name='withdraw-money-from-creator-wallet'),
     path('moniker-availability', MonikerAvailabilityAPIView.as_view(), name='moniker-availability'),
+    path('<str:address>', CreatorAPIView.as_view(), name='creator-detail'),
 ]
